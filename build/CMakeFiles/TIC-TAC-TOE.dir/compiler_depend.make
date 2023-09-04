c70281ec8356f0a8e11136920a96624fb461e3a8
Marion Ajijala <marion.ajijala@airmconsulting.com>
2023-09-04 15:51:32 -0600

    chore: untrack build dir

--- build/CMakeFiles/TIC-TAC-TOE.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for TIC-TAC-TOE.
-# This may be replaced when dependencies are built.
--- build/vendor/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/marionajijala/Tic-Tac-Toe/vendor/raylib/src
-# Build directory: /Users/marionajijala/Tic-Tac-Toe/build/vendor/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
