c5d576360b316e6fed0a5efcc8f4cc27e890ad6a
Marion Ajijala <marion.ajijala@airmconsulting.com>
2023-09-04 14:30:21 -0600

    initial commit

---
+++ build/CMakeFiles/TIC-TAC-TOE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TIC-TAC-TOE.
+# This may be replaced when dependencies are built.
---
+++ build/vendor/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/marionajijala/Tic-Tac-Toe/vendor/raylib/src
+# Build directory: /Users/marionajijala/Tic-Tac-Toe/build/vendor/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
